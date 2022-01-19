c6bea7e41b2204abb24515ce3ef7ed21d94eb816
RekGRpth <rekgrpth@gmail.com>
2022-01-19 11:56:42 +0500

    fix

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -2,12 +2,25 @@
 
 ln -fs libldap.a /usr/lib/libldap_r.a
 ln -fs libldap.so /usr/lib/libldap_r.so
-cd "$HOME/src/handlebars.c" && ./configure --disable-refcounting --disable-static && make -j"$(nproc)" install
-cd "$HOME/src/htmldoc" && ./configure --without-gui
-cd "$HOME/src/htmldoc/data" && make -j"$(nproc)" install
-cd "$HOME/src/htmldoc/fonts" && make -j"$(nproc)" install
-cd "$HOME/src/htmldoc/htmldoc" && make -j"$(nproc)" install
-cd "$HOME/src/libgraphqlparser" && cmake . && make -j"$(nproc)" install
-cd "$HOME/src/libinjection/src" && make -j"$(nproc)" install
-cd "$HOME/src/libjwt" && autoreconf -vif && ./configure && make -j"$(nproc)" install
-cd "$HOME/src/mustach" && make -j"$(nproc)" libs=single install
+cd "$HOME/src/handlebars.c"
+./configure --disable-refcounting --disable-static
+make -j"$(nproc)" install
+cd "$HOME/src/htmldoc"
+./configure --without-gui
+cd "$HOME/src/htmldoc/data"
+make -j"$(nproc)" install
+cd "$HOME/src/htmldoc/fonts"
+make -j"$(nproc)" install
+cd "$HOME/src/htmldoc/htmldoc"
+make -j"$(nproc)" install
+cd "$HOME/src/libgraphqlparser"
+cmake .
+make -j"$(nproc)" install
+cd "$HOME/src/libinjection/src"
+make -j"$(nproc)" install
+cd "$HOME/src/libjwt"
+autoreconf -vif
+./configure
+make -j"$(nproc)" install
+cd "$HOME/src/mustach"
+make -j"$(nproc)" libs=single install
